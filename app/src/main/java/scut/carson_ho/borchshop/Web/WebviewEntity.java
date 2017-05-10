3a3770a5e14667a5f567d43999ad04d9ff1d4d16
yany <yanzhikai_yjk@qq.com>
2017-05-10 16:36:58 +0800

    add exit Animation res

--- app/src/main/java/scut/carson_ho/borchshop/Web/WebviewEntity.java
+++ app/src/main/java/scut/carson_ho/borchshop/Web/WebviewEntity.java
@@ -26,8 +26,7 @@ import scut.carson_ho.borchshop.Utils.ScreenUtil;
 import scut.carson_ho.borchshop.Utils.SdcardConfig;
 
 /**
- * Created by Carson_Ho on 17/2/28.
- * @Description: webview工具类
+ * @Description: webview实体类
  */
 public class WebviewEntity extends WebView {
 
@@ -149,36 +148,6 @@ public class WebviewEntity extends WebView {
                     loadMessageUrl("file:///android_asset/error_http.html");
                 }
             }
-
-            //这里不用隐式Intent启动来一劳永逸，因为会清空Activity栈，导致不能正常一键退出，而且启动还很慢
-//            @Override
-//            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-//
-//                // 步骤2：根据协议的参数，判断是否是所需要的url
-//                // 根据scheme（协议格式） & authority（协议名）判断（前两个参数）
-//                // 首页的JS传入进来的 url = 'xmg://backToLoaclPage'（同时也是约定好的需要拦截的）
-//
-//                Uri uri = Uri.parse(url);
-//                // 如果url的协议 = 预先约定的 js 协议
-//                // 就解析往下解析参数
-//                if (uri.getScheme().equals("xmg")) {
-//                    // 如果 authority  = 预先约定协议里的 webview，即代表都符合约定的协议
-//                    // 所以拦截url,下面JS开始调用Android需要的方法
-//                    if (uri.getAuthority().equals("backToLoaclPage")) {
-//                        //  步骤3：
-//                        // 执行JS所需要调用的逻辑
-//                        System.out.println("js调用了Android的方法");
-//                        Intent localIntent = new Intent();
-////                        localIntent.setClass(context, GuiderActivity1.class);
-//                        localIntent.setAction("ACTION_BACK_TO_MAIN");
-//                        context.startActivity(localIntent);
-//
-//                    }
-//                    return true;
-//                }
-//                loadUrl(url);
-//                return super.shouldOverrideUrlLoading(view, url);
-//            }
         });
 
         setWebChromeClient(new WebChromeClient() {
